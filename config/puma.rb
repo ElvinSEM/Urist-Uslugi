5c01f275e0a130d7eb791fde36b91db1abfd1391
Elvin <elvin.seytibramov@gmail.com>
2026-04-06 20:33:11 +0300

    Fix Puma config for Render deployment

--- config/puma.rb
+++ config/puma.rb
@@ -1,32 +1,10 @@
-# config/puma.rb
-
-# Потоков на воркер
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
 threads threads_count, threads_count
 
-# Порт для Render или локально 3000
-port ENV.fetch("PORT") { 3000 }
+workers ENV.fetch("WEB_CONCURRENCY") { 0 } # single-mode для Render
 
-# Запуск в single-mode, если WEB_CONCURRENCY не задан
-workers ENV.fetch("WEB_CONCURRENCY") { 0 }  # 0 = single-mode
+port ENV.fetch("PORT") { 3000 } # порт от Render
 
-preload_app!  # улучшает перезапуск и память
+preload_app!
 
-# Разрешаем restart через bin/rails restart
 plugin :tmp_restart
-
-# Solid Queue supervisor, если нужно
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-
-# PID файл, если указан
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
-
-# Логи для деплоя
-on_worker_boot do
-  puts "Worker #{Process.pid} booted"
-  # Для ActiveRecord, если используется
-  if defined?(ActiveRecord)
-    ActiveRecord::Base.connection_pool.disconnect!
-    ActiveRecord::Base.establish_connection
-  end
-end
